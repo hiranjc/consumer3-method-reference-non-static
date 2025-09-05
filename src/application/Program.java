fd93d132b24a0af11dd14b81944cc03d00a5cf2f
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-05 18:22:16 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Locale;
+import java.util.List;
+import java.util.ArrayList;
+
+import entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		list.forEach(Product::nonStaticPriceUpdate);
+		list.forEach(System.out::println);
+	}
+}
